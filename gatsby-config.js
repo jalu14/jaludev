65207c3a0c9f0c1e5a7ec81675674addebd026c3
jalu <javidevictoria@gmail.com>
2020-10-28 22:26:47 +0100

    Netlify headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     plugins: [
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-sitemap`,
+        `gatsby-plugin-netlify`,
         {
             resolve: 'gatsby-transformer-remark',
             options: {
