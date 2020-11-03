c3503c068dcd74adbf1c0198e1be7df5defc1bea
jalu <javidevictoria@gmail.com>
2020-11-03 12:36:17 +0100

    adsense

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -20,6 +20,7 @@ export const Head = ({ title, children }: any) => {
         <Helmet title={title}>
             <html lang="es" />
             <meta name="google-site-verification" content="Q5hFA7PrMGkvUMPMQpwPVT_zT6AhbQFIq4tOxnC3Le0" />
+            <script data-ad-client="ca-pub-7944174490154863" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
             {children}
         </Helmet>
     )
